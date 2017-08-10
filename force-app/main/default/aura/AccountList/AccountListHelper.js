8555e0647396a0aa52e396de93ff95e87b649401
kawahara <kawahara@SB-RSVMAC-36.local>
2017-08-10 16:43:23 +0900

    Edit Lightinig Component

--- force-app/main/default/aura/AccountList/AccountListHelper.js
+++ force-app/main/default/aura/AccountList/AccountListHelper.js
@@ -1,9 +1,5 @@
 ({
-    doInit : function(component, event) {
-        var action = component.get("c.findAll");
-        action.setCallback(this, function(a) {
-          component.set("v.accounts", a.getReturnValue());
-        });
-        $A.enqueueAction(action);
+    helperMethod : function() {
+
     }
 })
